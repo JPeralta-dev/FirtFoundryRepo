93295e891bde1a47f34fbfb9987212160858c468
david123456858 <juandavidperaltafuentes@gmail.com>
2026-03-03 20:47:05 -0500

    feat: test complete

--- test/calculatorTest.t.sol
+++ test/calculatorTest.t.sol
@@ -11,7 +11,7 @@ contract calculatorTest is Test {
     uint256 public resultado = 100;
     address public admin = vm.addr(1); // esto ahce que genere una wallet que poderemos "manejar" desde nuestro wallet es decir
     // njosotros somos la EOA a Y el la b en eesta caso tendremos acceso por eso esta hecha para hcaer pruebas
-
+    address public RandomUser = vm.addr(2);
     function setUp() public {
         calculadora = new calculator(resultado, admin);
     } // se ejectua siempre antes de cualquier test
@@ -56,6 +56,51 @@ contract calculatorTest is Test {
 
         vm.expectRevert();
         calculadora.multiplier(firtNumber_, secondNumber_);
+    }
+
+    function testIfNotAdminCallDivisionRevert() public {
+        vm.startPrank(RandomUser); // IMPERSONAL UNA WALLET
+        uint256 firtNumber_ = 5;
+        uint256 secondNumber_ = 5;
+        vm.expectRevert();
+        // dentro de esa linea es como si estuviera llamando ese usuario
+        calculadora.division(firtNumber_, secondNumber_);
+        vm.stopPrank();
+    }
+
+    function testAdminCanCallDivisionCorrectly() public {
+        vm.startPrank(admin); // IMPERSONAL UNA WALLET
+        uint256 firtNumber_ = 5;
+        uint256 secondNumber_ = 5;
+
+        // dentro de esa linea es como si estuviera llamando ese usuario
+        calculadora.division(firtNumber_, secondNumber_);
+        vm.stopPrank();
+    }
+
+    function testDivision() public {
+        vm.startPrank(admin);
+        uint256 firtNumber_ = 5;
+        uint256 secondNumber_ = 5;
+
+        // dentro de esa linea es como si estuviera llamando ese usuario
+        uint256 result_ = calculadora.division(firtNumber_, secondNumber_);
+
+        assert(result_ == firtNumber_ / secondNumber_);
+        vm.stopPrank();
+    }
+
+    function testDivisionZero() public {
+        vm.startPrank(admin);
+
+        uint256 firtNumber_ = 5;
+        uint256 secondNumber_ = 0;
+
+        vm.expectRevert();
+        // dentro de esa linea es como si estuviera llamando ese usuario
+        calculadora.division(firtNumber_, secondNumber_);
+
+        vm.stopPrank();
     }
 
     //Fuzzing testing -> nivel alto, puesto para senior
